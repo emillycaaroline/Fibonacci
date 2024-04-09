b12960189ea4b9d5b2bbe14b63ab468de38e8b34
emillycaarolina <emillyjchagas@gmail.com>
2024-04-09 12:25:13 -0300

    testev2

--- seq.php
+++ seq.php
@@ -1,28 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body {
+            text-align: center;
+        }
+        p{
+            font-size: large;
+            font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        }
+    </style>
 </head>
+
 <body>
-<form action="" method="get">
-  Exibir até(maior que 2): <input type="number" name="number" /><br />
-  <input type="submit" name="submit" value="Calcular" />
- </form> 
- <?php 
-  $n=$_GET['number'];
-  $ultimo=1;
-  $penultimo=0;
-    
-  echo "0<br />1<br />";
-  for($count=1 ; $count<=$n-2 ; $count++){
-   $atual = $ultimo + $penultimo;
-   echo $atual."<br />";
-   
-   $penultimo = $ultimo;
-   $ultimo = $atual;
-  } 
- ?>
+    <form action="" method="get">
+      <p>Exibir até(maior que 2):</p>   <input type="number" name="number" /><br /> <br>
+        <input type="submit" name="submit" value="Calcular" /> <br> <br>
+    </form>
+    <?php
+    $n = $_GET['number'];
+    $ultimo = 1;
+    $penultimo = 0;
+
+    echo "0<br />1<br />";
+    for ($count = 1; $count <= $n - 2; $count++) {
+        $atual = $ultimo + $penultimo;
+        echo $atual . "<br />";
+
+        $penultimo = $ultimo;
+        $ultimo = $atual;
+    }
+    ?>
 </body>
+
 </html>
